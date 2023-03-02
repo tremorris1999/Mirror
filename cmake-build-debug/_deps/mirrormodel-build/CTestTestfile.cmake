3d35d39c1b82924009b39e004fdd1b3ec8e39579
Trevor Morris <tremorris1999@gmail.com>
2023-03-02 08:22:52 +0000

    fix: cleanup cached files

--- cmake-build-debug/_deps/mirrormodel-build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Trevor/CLionProjects/Mirror/cmake-build-debug/_deps/mirrormodel-src
-# Build directory: C:/Users/Trevor/CLionProjects/Mirror/cmake-build-debug/_deps/mirrormodel-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("mirrormod")
-subdirs("tests")
--- cmake-build-debug/_deps/mirrormodel-build/mirrormod/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Trevor/CLionProjects/Mirror/cmake-build-debug/_deps/mirrormodel-src/mirrormod
-# Build directory: C:/Users/Trevor/CLionProjects/Mirror/cmake-build-debug/_deps/mirrormodel-build/mirrormod
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
